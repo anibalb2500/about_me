77d270bd85f31746ca8e373da36b8a8e151ea503
Anibal Bojorquez <anibal-bojorquez@sandhills.com>
2022-01-30 19:42:24 -0700

    Added Data Binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,26 +8,32 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.done_button)
-        button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nick_name_edit)
-        val nickName = findViewById<TextView>(R.id.nick_name_text)
-
-        nickName.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickName.visibility = View.VISIBLE
+        binding.apply {
+            nickNameText.text = binding.nickNameEdit.text
+            invalidateAll()
+            nickNameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nickNameText.visibility = View.VISIBLE
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
